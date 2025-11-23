f4d6f6d9ae04d9879ca7a06a0e4458df333d19a8
algieba <algieba.king@gmail.com>
2025-11-23 23:40:28 +0800

    feat: initailize AutoOpponentWrapper
    1. implemented the AutoOpponentWrapper
    2. implemented partial tests

--- games/AutoOpponentWrapper.py
+++ games/AutoOpponentWrapper.py
@@ -1,4 +1,94 @@
+from __future__ import annotations
+from typing import Dict, Tuple, Any, TYPE_CHECKING
 import gymnasium as gym
 
 
-class AutoOpponentWrapper(gym.Wrapper): ...
+if TYPE_CHECKING:
+    from GameAgent import GameAgent
+    from OinkGame import OinkGameEnv
+
+
+class AutoOpponentWrapper(gym.Wrapper):
+    def __init__(
+        self, env: OinkGameEnv, bots: Dict[int, GameAgent], ego_player_idx: int = 0
+    ):
+        super().__init__(env)
+        self.bots = bots
+        self.ego_player_idx = ego_player_idx
+
+    @staticmethod
+    def __need_process_opponent_turns(
+        terminated: bool, truncted: bool, current_player_idx: int, ego_player_idx: int
+    ) -> bool:
+        return not terminated and not truncted and current_player_idx != ego_player_idx
+
+    def __process_opponent_turns(
+        self,
+        previous_observation: Any,
+        terminated: bool,
+        truncated: bool,
+        previous_info: Dict[str, Any],
+    ) -> Tuple[Any, float, bool, bool, Dict[str, Any]]:
+        observation = previous_observation
+        info = previous_info
+        while self.__need_process_opponent_turns(
+            terminated=terminated,
+            truncted=truncated,
+            current_player_idx=self.env.current_player_idx,
+            ego_player_idx=self.ego_player_idx,
+        ):
+            current_player_idx = self.env.current_player_idx
+            bot = self.bots[current_player_idx]
+            bot_observation = self.env.unwrapped._get_observation(current_player_idx)
+            bot_action_mask = self.env.unwrapped._get_action_mask(current_player_idx)
+            bot_action = bot.predict(
+                observation=bot_observation, action_mask=bot_action_mask
+            )
+            observation, reward, terminated, truncated, info = self.env.step(
+                action=bot_action
+            )
+        return observation, reward, terminated, terminated, info
+
+    def reset(self, **kwargs):
+        observation, info = self.env.reset(**kwargs)
+        terminated = False
+        truncated = False
+
+        # check once after reset ego player is not the current
+        while self.__need_process_opponent_turns(
+            terminated=terminated,
+            truncted=truncated,
+            current_player_idx=self.env.current_player_idx,
+            ego_player_idx=self.ego_player_idx,
+        ):
+            # let it move til current player is ego player, no need to know bot's score
+            observation, _, terminated, truncated, info = self.__process_opponent_turns(
+                previous_observation=observation,
+                terminated=terminated,
+                truncated=terminated,
+                previous_info=info,
+            )
+        return observation, info
+
+    def step(self, action: Any):
+
+        # the step is now only for ego player movement
+        observation, reward, terminated, truncated, info = self.env.step(action=action)
+
+        while self.__need_process_opponent_turns(
+            terminated=terminated,
+            truncted=truncated,
+            current_player_idx=self.env.current_player_idx,
+            ego_player_idx=self.ego_player_idx,
+        ):
+            # let bots run
+            observation, reward, terminated, truncated, info = (
+                self.__process_opponent_turns(
+                    previous_observation=observation,
+                    terminated=terminated,
+                    truncated=truncated,
+                    previous_info=info,
+                )
+            )
+
+        return observation, reward, terminated, truncated, info
--- tests/test_auto_opponent_wrapper.py
+++ tests/test_auto_opponent_wrapper.py
@@ -3,6 +3,222 @@ import gymnasium as gym
 import numpy as np
 
 from games.AutoOpponentWrapper import AutoOpponentWrapper
+from games.OinkGame import OinkGameEnv
+from games.GameAgent import GameAgent
 
 
-class TestAutoOpponentWrapper: ...
+class MockBot(GameAgent):
+    def __init__(self):
+        self.call_count = 0
+
+    def predict(self, observation, action_mask):
+        self.call_count += 1
+        return 1
+
+
+class ScriptedEnv(OinkGameEnv):
+    def __init__(
+        self,
+        player_sequence: list[int],
+        reward_sequence: list[int] = None,
+        render_mode=None,
+    ):
+        super().__init__(render_mode)
+        self.__player_sequence = player_sequence
+        self.__reward_sequnce = (
+            reward_sequence
+            if reward_sequence is not None
+            else [1 for _ in self.__player_sequence]
+        )
+        self.action_space = gym.spaces.Discrete(2)
+        self.observation_space = gym.spaces.Discrete(10)
+        self.current_sequence_id = 0
+        self.current_player_idx = self.__player_sequence[self.current_sequence_id]
+
+    def _apply_action(self, action):
+        reward = self.__reward_sequnce[self.current_sequence_id]
+        if self.current_sequence_id == len(self.__player_sequence) - 1:
+            return reward, True
+
+        self.current_sequence_id += 1
+        self.current_player_idx = self.__player_sequence[self.current_sequence_id]
+        return reward, False
+
+    def _get_action_mask(self, player_idx):
+        return [1]
+
+    def _render_text(self):
+        return ""
+
+    def _get_global_state(self):
+        return {1: "0"}
+
+    def _get_observation(self, player_idx):
+        return ""
+
+    def _reset_logic(self, seed=0, options=None):
+        self.current_sequence_id = 0
+        self.current_player_idx = self.__player_sequence[self.current_sequence_id]
+
+
+class TestAutoOpponentWrapper:
+
+    def test_regular_step(self):
+        bot = MockBot()
+        bots = {
+            1: bot,
+        }
+        player_sequence = [1, 0, 0]
+        reward_sequence = [1, 2, 3]
+        env = ScriptedEnv(
+            player_sequence=player_sequence, reward_sequence=reward_sequence
+        )
+        wrappered_env = AutoOpponentWrapper(env=env, bots=bots, ego_player_idx=0)
+        wrappered_env.reset()
+
+        _, reward, terminated, _, _ = wrappered_env.step(action=1)
+        assert reward == reward_sequence[1]
+        assert not terminated
+        assert bot.call_count == 1
+
+        _, reward, terminated, _, _ = wrappered_env.step(action=1)
+        assert reward == reward_sequence[2]
+        assert terminated
+        assert bot.call_count == 1
+
+    def test_regular_reset(self):
+        bot = MockBot()
+        bots = {
+            1: bot,
+        }
+        env = ScriptedEnv(player_sequence=[1, 0, 0])
+        wrappered_env = AutoOpponentWrapper(env=env, bots=bots, ego_player_idx=0)
+        wrappered_env.reset()
+        _, _, terminated, _, _ = wrappered_env.step(action=1)
+        assert not terminated
+        _, _, terminated, _, _ = wrappered_env.step(action=1)
+        assert terminated
+        wrappered_env.reset()
+        _, _, terminated, _, _ = wrappered_env.step(action=1)
+        assert not terminated
+
+    def test_opponent_starts_game(self):
+        bot = MockBot()
+        bots = {
+            1: bot,
+        }
+        env = ScriptedEnv(player_sequence=[1, 0])
+        wrappered_env = AutoOpponentWrapper(env=env, bots=bots, ego_player_idx=0)
+        wrappered_env.reset()
+        _, _, terminated, _, _ = wrappered_env.step(action=1)
+        assert terminated
+        assert env.current_player_idx == 0
+        assert bot.call_count == 1
+
+    def test_opponent_starts_win(self):
+        # no exception raised and no death loop should be here
+        bot = MockBot()
+        bots = {
+            1: bot,
+        }
+        env = ScriptedEnv(player_sequence=[1])
+        wrappered_env = AutoOpponentWrapper(env=env, bots=bots, ego_player_idx=0)
+        wrappered_env.reset()
+        _, _, terminated, _, _ = wrappered_env.step(action=1)
+        assert terminated
+
+    @pytest.mark.skip("TODO")
+    def test_ego_idx(self): ...  # TODO
+
+    @pytest.mark.skip("TODO")
+    def test_bots(self):  # TODO
+        # empty bots
+        # wrong idx bots
+        # wrong bots type
+        ...
+
+    test_players_params = [
+        {
+            "id": "ego turn only case",
+            "player_sequence": [0, 0, 0, 0],
+            "bots": {1: MockBot()},
+            "reward_sequence": [1, 2, 3, 4],
+        },
+        {
+            "id": "long ego turn only case",
+            "player_sequence": [0 for _ in range(5000)],
+            "bots": {1: MockBot()},
+            "reward_sequence": [i**2 for i in range(5000)],
+        },
+        # {
+        #     "id": "",
+        #     "player_sequence": [],
+        #     "bots": {1: MockBot()},
+        #     "reward_sequence": [],
+        # },
+    ]
+
+    @pytest.mark.parametrize(
+        "case", test_players_params, ids=[param["id"] for param in test_players_params]
+    )
+    def test_players_play(self, case):
+        expected_ego_movement_counts = len(case["player_sequence"])
+        expected_ego_rewards = [
+            reward
+            for player_idx, reward in zip(
+                case["player_sequence"], case["reward_sequence"]
+            )
+            if player_idx == 0
+        ]
+
+        expected_bot_movement_counts_dict = {}
+        for bot_idx in case["bots"]:
+            expected_bot_movement_counts_dict[bot_idx] = case["player_sequence"].count(
+                bot_idx
+            )
+        env = ScriptedEnv(
+            player_sequence=case["player_sequence"],
+            reward_sequence=case["reward_sequence"],
+        )
+        wrappered_env = AutoOpponentWrapper(env=env, bots=case["bots"])
+        wrappered_env.reset()
+
+        # move
+        for i in range(expected_ego_movement_counts):
+            _, reward, terminated, _, _ = wrappered_env.step(action=0)
+
+            if i == expected_ego_movement_counts - 1:
+                assert terminated
+            else:
+                assert not terminated
+            assert (
+                env.current_player_idx == 0
+            )  # for wrappered environment, will only be players view, all other bot's turns will be collapse
+
+            assert reward == expected_ego_rewards[i]
+
+        for bot_idx, bot in case["bots"].items():
+            assert bot.call_count == expected_bot_movement_counts_dict[bot_idx]
+
+    def test_ego_turn_only(self):
+        bot = MockBot()
+        bots = {
+            1: bot,
+        }
+        player_sequence = [0, 0, 0, 0]
+        reward_sequence = [0, 0, 0, 0]
+        env = ScriptedEnv(
+            player_sequence=player_sequence, reward_sequence=reward_sequence
+        )
+        wrappered_env = AutoOpponentWrapper(env=env, bots=bots)
+        wrappered_env.reset()
+
+        for i, player_idx in enumerate(player_sequence):
+            _, _, terminated, _, _ = wrappered_env.step(action=0)
+
+            if i == len(player_sequence) - 1:
+                assert terminated
+            else:
+                assert not terminated
+            assert env.current_player_idx == player_idx
+            assert bot.call_count == 0
