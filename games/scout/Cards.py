5866da992c93a5954ba41ef1c8b87c6344f5e513
algieba <algieba.king@gmail.com>
2025-11-25 22:23:11 +0800

    feat: implemente scout card

---
+++ games/scout/Cards.py
@@ -0,0 +1,30 @@
+from typing import List, Self
+
+
+class Card:
+    def __init__(self, top: int, bottom: int, supported_players: List[int]):
+        self.__top = top
+        self.__bottom = bottom
+        self.__supported_players = supported_players
+
+    @property
+    def top(self):
+        return self.__top
+
+    @property
+    def bottom(self):
+        return self.__bottom
+
+    @property
+    def supported_players(self):
+        return self.__supported_players
+
+    def flip(self) -> Self:
+        self.__top, self.__bottom = self.__bottom, self.__top
+        return self
+
+
+class Cards: ...
+
+
+class CardInitilizer: ...
---
+++ tests/scout/test_card.py
@@ -0,0 +1,48 @@
+import pytest
+from games.scout.Cards import Card
+
+
+class TestCard:
+    def card(self):
+        return Card(top=1, bottom=2, supported_players=[2, 3, 4, 5])
+
+    def test_card_attribution(self):
+        card = self.card()
+        assert card.top == 1
+        assert card.bottom == 2
+
+    def test_card_flip(self):
+        card = self.card()
+        fliped_card = card.flip()
+        assert card.top == 2
+        assert card.bottom == 1
+        assert fliped_card.top == 2
+        assert fliped_card.bottom == 1
+
+    @pytest.mark.parametrize(
+        argnames="supported_players",
+        argvalues=[[2, 3, 4, 5], [2, 4, 5], [5]],
+        ids=["2,3,4,5", "2,4,5", "5"],
+    )
+    def test_card_support_player(self, supported_players):
+        card = Card(top=1, bottom=2, supported_players=supported_players)
+        assert 1 not in card.supported_players
+        for supported_num in supported_players:
+            assert supported_num in card.supported_players
+
+    def test_immutable_properties(self):
+        card = self.card()
+        with pytest.raises(
+            AttributeError,
+            match="property 'top' of 'Card' object has no setter",
+        ):
+            card.top = 10
+        with pytest.raises(
+            AttributeError, match="property 'bottom' of 'Card' object has no setter"
+        ):
+            card.bottom = 6
+        with pytest.raises(
+            AttributeError,
+            match="property 'supported_players' of 'Card' object has no setter",
+        ):
+            card.supported_players = [2, 3, 4, 5]
